7229cd50555cd50aa312e89d9b8c6fe4abe62531
sinchiyap <sinchiyap@gmail.com>
2016-03-25 12:33:21 +0800

    Added files via upload

---
+++ javascripts/feedback.js
@@ -0,0 +1,69 @@
+
+$(document).ready(function() {
+    $("#show1").click(function() {
+        $("#ironman").toggle('show');
+    });
+});
+$(document).ready(function() {
+    $("#show2").click(function() {
+        $("#tih").toggle('show');
+    });
+});
+$(document).ready(function() {
+    $("#show3").click(function() {
+        $("#im2").toggle('show');
+    });
+});
+$(document).ready(function() {
+    $("#show4").click(function() {
+        $("#thor").toggle('show');
+    });
+});
+$(document).ready(function() {
+    $("#show5").click(function() {
+        $("#cap").toggle('show');
+    });
+});
+$(document).ready(function() {
+    $("#show6").click(function() {
+        $("#avengers").toggle('show');
+    });
+});
+$(document).ready(function() {
+    $("#show7").click(function() {
+        $("#im3").toggle('show');
+    });
+});
+$(document).ready(function() {
+    $("#show8").click(function() {
+        $("#thor2").toggle('show');
+    });
+});
+$(document).ready(function() {
+    $("#show9").click(function() {
+        $("#cap2").toggle('show');
+    });
+});
+$(document).ready(function() {
+    $("#show10").click(function() {
+        $("#gotg").toggle('show');
+    });
+});
+$(document).ready(function() {
+    $("#show11").click(function() {
+        $("#avengers2").toggle('show');
+    });
+});
+$(document).ready(function() {
+    $("#show12").click(function() {
+        $("#antman").toggle('show');
+    });
+});
+//$(document).ready(function() {
+//  $('strong').hover(function() {
+//    $('#sidepic').css('display', 'block');
+//  }, function() {
+// on mouseout, reset
+//    $('#sidepic').css('display', '');
+//  });
+//});
